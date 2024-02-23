03c76187fce29a40f778aa2801b2c5e33eb81444
Yingtai Xiao <1025965245@qq.com>
2024-02-23 10:30:46 -0500

    update

---
+++ brazil/small_example.py
@@ -0,0 +1,18 @@
+# import the CM_rand.py in the brazil directory
+from CM_rand import config, CM
+import numpy as np
+
+args = config()
+# Mechanism 1: (B1, S1)
+# Mechanism 2: (B2, S2)
+B1 = np.array([[1, 1, 1, 0, 0, 0],
+               [0, 0, 0, 1, 1, 1]])
+S1 = np.eye(2)
+B2 = np.array([[1, 0, 0, 1, 0, 0],
+               [0, 1, 0, 0, 1, 0],
+               [0, 0, 1, 0, 0, 1]])
+S2 = np.eye(3)
+
+com_mech = CM(args, B1, S1, B2, S2)
+B_com = com_mech.Bc
+S_com = com_mech.Sc
